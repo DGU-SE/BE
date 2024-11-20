f16b8ac9c6e6afd09dc3bfd9536c4959778b923d
cyycyy77 <yoonyoul0710@gmail.com>
2024-11-20 17:17:53 +0900

    check CommentDTO

--- src/main/java/com/twofullmoon/howmuchmarket/dto/CommentDTO.java
+++ src/main/java/com/twofullmoon/howmuchmarket/dto/CommentDTO.java
@@ -1,15 +1,5 @@
 package com.twofullmoon.howmuchmarket.dto;
 
-import lombok.*;
-
-@Getter
-@Setter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class CommentDTO {
-    private Integer productId;
-    private String userId;
-    private String content;
-    private boolean isSecret;
+
 }
